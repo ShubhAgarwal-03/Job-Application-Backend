3cfb3cf3d78fe23ce1e700fc8e4b47cdf0b03836
Shubh Agarwal <shubhagarwal031@gmail.com>
2026-04-13 05:01:47 +0530

    Update applicationController.js

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,5 +1,5 @@
 const Application = require("../models/application");
-const Job = require("../models/Job");
+const Job = require("../models/job");
 const Resume = require("../models/resume");
 const User = require("../models/user");
 const {
